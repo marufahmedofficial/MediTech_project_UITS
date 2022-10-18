450af8b3757eebfa6d03566b50cfd61b7aa26d4b
maruf <mk6116055@gmail.com>
2022-10-18 21:49:00 +0600

    started homepage and appbar

---
+++ lib/pages/homepage.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+class HomePage extends StatefulWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('MediTech'),
+        centerTitle: true,
+      ),
+    );
+  }
+}
