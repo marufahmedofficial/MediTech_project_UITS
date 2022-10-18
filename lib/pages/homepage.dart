a427418a06ae7864811a36deb252377bad0a1282
maruf <mk6116055@gmail.com>
2022-10-19 00:24:02 +0600

    floating button

--- lib/pages/homepage.dart
+++ lib/pages/homepage.dart
@@ -15,6 +15,25 @@ class _HomePageState extends State<HomePage> {
           title: const Text('MediTech'),
           centerTitle: true,
         ),
+      floatingActionButton: FloatingActionButton(
+        // backgroundColor: Colors.indigoAccent,
+        // splashColor: Colors.purple,
+        // hoverColor: Colors.orange,
+
+        onPressed: (){},
+        child: Icon(Icons.medical_information),
+        backgroundColor: Colors.red.shade700,
+        foregroundColor: Colors.white,
+        elevation: 10.0,
+        shape: BeveledRectangleBorder(
+          borderRadius: BorderRadius.circular(20.0),
+          side: BorderSide()
+        ),
+
+
+        // // label : const Text('Change'),
+        // icon: const Icon(Icons.change_circle) ,
+      ),
       body: Center(
         child: Text('This project is successfully running..'),
       ),
