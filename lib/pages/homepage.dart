369d69ff62bc0f9fe604548158d6ce1b199cb230
maruf <mk6116055@gmail.com>
2022-10-19 00:10:08 +0600

    homepage running

--- lib/pages/homepage.dart
+++ lib/pages/homepage.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+
 class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
 
@@ -10,10 +11,15 @@ class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('MediTech'),
-        centerTitle: true,
+        appBar: AppBar(
+          title: const Text('MediTech'),
+          centerTitle: true,
+        ),
+      body: Center(
+        child: Text('This project is successfully running..'),
       ),
+
+
     );
   }
 }
