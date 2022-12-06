59f6f3bbaa291790515dc845bbb83374eeca82a8
Maruf Ahmed <mk6116055@gmail.com>
2022-12-07 03:32:15 +0600

    hospital listing --

--- lib/pages/hospital/hospital.dart
+++ lib/pages/hospital/hospital.dart
@@ -4,7 +4,6 @@ import 'package:nb_utils/nb_utils.dart';
 
 
 class Hospital extends StatefulWidget {
-  static String tag = '/DemoCWSlidingSegmentedControlScreen';
 
   @override
   HospitalState createState() => HospitalState();
@@ -18,7 +17,7 @@ class HospitalState extends State<Hospital> {
   }
 
   init() async {
-    //
+
   }
 
   int segmentedControlGroupValue = 0;
@@ -32,17 +31,50 @@ class HospitalState extends State<Hospital> {
 
   @override
   Widget build(BuildContext context) {
-    Widget mPage(var value) {
+    Widget mPage1(var value) {
       return Column(
         crossAxisAlignment: CrossAxisAlignment.center,
         children: [
-          Text(
-            value,
+          Text(value,
             style: boldTextStyle(),
           ).paddingLeft(16),
           Padding(
             padding: EdgeInsets.only(left: 20, right: 20, top: 16),
             child: Text('Maruf AHmed',
+              style: secondaryTextStyle(),
+              textAlign: TextAlign.justify,
+            ),
+          ),
+        ],
+      );
+    }
+    Widget mPage2(var value) {
+      return Column(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          Text(value,
+            style: boldTextStyle(),
+          ).paddingLeft(16),
+          Padding(
+            padding: EdgeInsets.only(left: 20, right: 20, top: 16),
+            child: Text('Maruf',
+              style: secondaryTextStyle(),
+              textAlign: TextAlign.justify,
+            ),
+          ),
+        ],
+      );
+    }
+    Widget mPage3(var value) {
+      return Column(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          Text(value,
+            style: boldTextStyle(),
+          ).paddingLeft(16),
+          Padding(
+            padding: EdgeInsets.only(left: 20, right: 20, top: 16),
+            child: Text('Maruf',
               style: secondaryTextStyle(),
               textAlign: TextAlign.justify,
             ),
@@ -84,9 +116,9 @@ class HospitalState extends State<Hospital> {
                     });
                   }),
             ),
-            if (_sliding == 0) mPage("Government"),
-            if (_sliding == 1) mPage("Private"),
-            if (_sliding == 2) mPage("Covid"),
+            if (_sliding == 0) mPage1("Government"),
+            if (_sliding == 1) mPage2("Private"),
+            if (_sliding == 2) mPage3("Covid"),
           ],
         ),
       ),
@@ -94,23 +126,3 @@ class HospitalState extends State<Hospital> {
   }
 }
 
-AppStore appStore = AppStore();
-class AppStore{
-
-  Color textPrimaryColor = Color(0xFF212121);
-  Color iconColorPrimaryDark = Color(0xFF212121);
-  Color scaffoldBackground = Color(0xFFEBF2F7);
-  Color backgroundColor = Colors.black;
-  Color backgroundSecondaryColor = Color(0xFF131d25);
-  Color appColorPrimaryLightColor = Color(0xFFF9FAFF);
-  Color textSecondaryColor = Color(0xFF5A5C5E);
-  Color appBarColor = Colors.white;
-  Color iconColor = Color(0xFF212121);
-  Color iconSecondaryColor = Color(0xFFA8ABAD);
-  Color cardColor = Colors.white;
-  Color appColorPrimary = Color(0xFF1157FA);
-  Color scaffoldBackgroundColor = Color(0xFFEFEFEF);
-
-  AppStore();
-
-}
