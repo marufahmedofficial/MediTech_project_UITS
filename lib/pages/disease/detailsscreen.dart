91a23e48b28df1db4bdd8c39cb6e9dc6647495a8
Maruf Ahmed <mk6116055@gmail.com>
2022-12-09 04:07:11 +0600

    disease details

---
+++ lib/pages/disease/detailsscreen.dart
@@ -0,0 +1,55 @@
+import 'package:flutter/material.dart';
+import 'package:meditech/pages/disease/data/constants.dart';
+import 'package:meditech/pages/disease/data/tools.dart';
+import 'package:meditech/pages/disease/resultsscreen.dart';
+
+ 
+class DetailsScreen extends StatelessWidget {
+  int index;
+  DetailsScreen({required this.index,Key? key}) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    List resultsReferance = ["abc","abcd"];
+    resultsReferance.addAll(results[index]["symptoms"]);
+    return Scaffold(
+      appBar: Constants.appBar,
+      body: ListView.builder(itemCount: resultsReferance.length,itemBuilder: (context,i){
+        if(i==0){
+          return Padding(
+            padding: const EdgeInsets.all(21.0),
+            child: Text(results[index]["name"].toString(),style: TextStyle(fontSize: 25),),
+          );
+        }else if(i==1){
+          return Padding(
+            padding: const EdgeInsets.all(14.0),
+            child: Text(results[index]["explanation"],style: TextStyle(fontSize: 15),),
+          );
+        }else{
+          return ListTile(
+            leading: Text(resultsReferance[i],style: TextStyle(fontSize: 15),),
+            trailing: Tools.tickOrCross(element:resultsReferance[i])==true?Icon(Icons.check):Icon(Icons.close)
+          );
+        }
+      })
+    );
+  }
+}
+/*
+(
+        children: [
+          Padding(
+            padding: const EdgeInsets.all(21.0),
+            child: Text(results[index]["name"].toString(),style: TextStyle(fontSize: 25),),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(14.0),
+            child: Text(results[index]["explanation"],style: TextStyle(fontSize: 15),),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(10.0),
+            child: Text(symptomsOfThisDisease,style: TextStyle(fontSize: 15),),
+          ),
+
+        ],
+      )
+*/
