91321ba5c25b16a84d5dc616e1fc908f79a0adf4
Maruf Ahmed <mk6116055@gmail.com>
2022-12-28 01:45:31 +0600

    donation page

--- lib/pages/homepage.dart
+++ lib/pages/homepage.dart
@@ -490,6 +490,20 @@ class _HomePageState extends State<HomePage> {
                   name: 'Blood Bank'),
             ),
             InkWell(
+              onTap: () {
+                Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                        builder: (context) => const BloodMain()));
+              },
+              mouseCursor: MouseCursor.defer,
+              splashColor: Colors.red.shade100,
+              hoverColor: Colors.grey.shade200,
+              child: _pageadd(
+                  image: 'assets/images/b_donation.png',
+                  name: 'Blood Donate'),
+            ),
+            InkWell(
               onTap: () {},
               mouseCursor: MouseCursor.defer,
               splashColor: Colors.red.shade100,
