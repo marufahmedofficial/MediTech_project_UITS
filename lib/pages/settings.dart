1f94c8b0156d46bc7cb51d501987961f6f57cdcf
Maruf Ahmed <mk6116055@gmail.com>
2022-12-28 09:47:51 +0600

    Settings page

--- lib/pages/settings.dart
+++ lib/pages/settings.dart
@@ -18,55 +18,78 @@ class _SettingsState extends State<Settings> {
         centerTitle: true,
       ),
       body:
-      SettingsList(
-        sections: [
-          SettingsSection(
-            title: Text('Common'),
-            tiles: <SettingsTile>[
-              SettingsTile.navigation(
-                leading: Icon(Icons.language),
-                title: Text('Language'),
-                value: Text('English'),
-              ),
-              SettingsTile.switchTile(
-                onToggle: (value) {},
-                initialValue: true,
-                leading: Icon(Icons.format_paint),
-                title: Text('Enable Dark theme'),
-              ),
-              SettingsTile.switchTile(
-                onToggle: (value) {},
-                initialValue: false,
-                leading: Icon(Icons.notifications_active_rounded),
-                title: Text('Enable Notifications'),
-              ),
-            ],
-          ),
-          SettingsSection(
-            title: Text('Security'),
-            tiles: [
-              SettingsTile(
-                title: Text('Password'),
-                value: Text('App Password'),
-                leading: Icon(Icons.lock),
-                onPressed: (BuildContext context) {},
-              ),
+      Container(
+        child: SettingsList(
+          sections: [
+            SettingsSection(
+              title: Text('Common'),
+              tiles: <SettingsTile>[
+                SettingsTile.navigation(
+                  leading: Icon(Icons.language),
+                  title: Text('Language'),
+                  value: Text('English'),
+                ),
+                SettingsTile.switchTile(
+                  onToggle: (value) {},
+                  initialValue: true,
+                  leading: Icon(Icons.format_paint),
+                  title: Text('Enable Dark theme'),
+                ),
+                SettingsTile.switchTile(
+                  onToggle: (value) {},
+                  initialValue: false,
+                  leading: Icon(Icons.notifications_active_rounded),
+                  title: Text('Enable Notifications'),
+                ),
+              ],
+            ),
+            SettingsSection(
+              title: Text('Security'),
+              tiles: [
+                SettingsTile(
+                  title: Text('Password'),
+                  value: Text('App Password'),
+                  leading: Icon(Icons.lock),
+                  onPressed: (BuildContext context) {},
+                ),
 
-               SettingsTile(title: Text('FingerPrint'),
-                 leading: Icon(Icons.fingerprint),
-                 value: Text('Use Fingerprint to open app'),
-               )
+                 SettingsTile(title: Text('FingerPrint'),
+                   leading: Icon(Icons.fingerprint),
+                   value: Text('Use Fingerprint to open app'),
+                 )
 
-              // SettingsTile.switchTile(
-              //   title: 'Use fingerprint',
-              //   leading: Icon(Icons.fingerprint),
-              //   switchValue: true,
-              //   onToggle: (value) {},
-              // ),
-            ],
-          ),
+                // SettingsTile.switchTile(
+                //   title: 'Use fingerprint',
+                //   leading: Icon(Icons.fingerprint),
+                //   switchValue: true,
+                //   onToggle: (value) {},
+                // ),
+              ],
+            ),
+            SettingsSection(
+              title: Text('Account'),
+              tiles: [
+                SettingsTile(
+                  title: Text('Phone'),
+                  leading: Icon(Icons.phone),
+                  onPressed: (BuildContext context) {},
+                ),
+                SettingsTile(
+                  title: Text('Email'),
+                  leading: Icon(Icons.email),
+                  onPressed: (BuildContext context) {},
+                ),
+                SettingsTile(
+                  title: Text('Sign Out'),
+                  leading: Icon(Icons.logout_rounded),
+                  onPressed: (BuildContext context) {},
+                ),
 
-        ],
+              ],
+            ),
+
+          ],
+        ),
       ),
     );
   }
