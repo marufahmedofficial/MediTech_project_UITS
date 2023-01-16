4b33e6c67b6a23e6398b0811c867e1b943b060d0
Maruf Ahmed <mk6116055@gmail.com>
2023-01-16 07:48:22 +0600

    profile

--- lib/pages/profile/user/user_data.dart
+++ lib/pages/profile/user/user_data.dart
@@ -9,10 +9,10 @@ class UserData {
 
   static User myUser = User(
     image:
-        "https://upload.wikimedia.org/wikipedia/en/0/0b/Darth_Vader_in_The_Empire_Strikes_Back.jpg",
-    name: 'Test Test',
-    email: 'test.test@gmail.com',
-    phone: '(208) 206-5039',
+        "https://img.freepik.com/premium-vector/heart-medical-shape-doctor-logo-design_23987-717.jpg",
+    name: 'Your Name',
+    email: 'example@.com',
+    phone: '+880........',
   );
 
   static Future init() async =>
