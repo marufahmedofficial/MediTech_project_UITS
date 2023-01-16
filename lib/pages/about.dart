97578483bf32d684bd6a279eea3079b10312accf
Maruf Ahmed <mk6116055@gmail.com>
2023-01-17 03:02:24 +0600

    about page

--- lib/pages/about.dart
+++ lib/pages/about.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'homepage.dart';
 
@@ -12,11 +13,11 @@ class _AboutPageState extends State<AboutPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.teal.shade900,
+      backgroundColor: Colors.white,
       appBar: AppBar(
         backgroundColor: Colors.transparent,
         iconTheme: IconThemeData(
-            color: Colors.white),
+            color: Colors.black),
         leading: BackButton(
           onPressed:(){Navigator.push(
               context,
@@ -31,17 +32,17 @@ class _AboutPageState extends State<AboutPage> {
           children: <Widget>[
             CircleAvatar(
               backgroundImage: AssetImage('assets/images/appstore.png'),
-              radius: 60.0,
+              radius: 50.0,
             ),
             SizedBox(
               height: 20,
             ),
-            Text('MediTech App',
+            Text('Project MediTech',
               style: TextStyle(
                 fontFamily: 'Pacifico',
-                color: Colors.white,
+                color: Colors.black,
                 fontWeight: FontWeight.bold,
-                fontSize: 40.0,
+                fontSize: 30.0,
               ),
             ),
             SizedBox(
@@ -50,9 +51,9 @@ class _AboutPageState extends State<AboutPage> {
             Text(
               'Made with Flutter',
               style: TextStyle(
-                letterSpacing: 2.0,
-                color: Colors.white,
-                fontSize: 20.0,
+                letterSpacing: 3.0,
+                color: Colors.black,
+                fontSize: 14.0,
               ),
             ),
             SizedBox(
@@ -63,88 +64,71 @@ class _AboutPageState extends State<AboutPage> {
                 thickness: 4.0,
               ),
             ),
-            // Card(
-            //   color: Colors.white,
-            //   margin: EdgeInsets.symmetric(vertical: 15.0,horizontal: 30.0),
-            //   child: ListTile(
-            //     leading:  Icon(
-            //       Icons.android,
-            //       color: Colors.teal,
-            //       size: 30.0,
-            //     ),
-            //     title: Text(
-            //       'Version : 1.0.5',
-            //       style: TextStyle(
-            //         fontSize: 22.0,
-            //         color: Colors.teal,
-            //         fontWeight: FontWeight.bold,
-            //       ),
-            //     ),
-            //   ),
-            // ),
-            // Card(
-            //   color: Colors.white,
-            //   margin: EdgeInsets.symmetric(vertical: 10.0,horizontal: 30.0),
-            //   child: ListTile(
-            //     leading:  Icon(
-            //       Icons.email,
-            //       color: Colors.teal,
-            //       size: 30.0,
-            //     ),
-            //     title: Text(
-            //       'maruf.c.uits@gmail.com',
-            //       style: TextStyle(
-            //         fontSize: 20.0,
-            //         color: Colors.teal,
-            //         fontWeight: FontWeight.bold,
-            //       ),
-            //     ),
-            //   ),
-            // ),
+
             ListTile(
-              title: const Text('APP_NAME'),
-              subtitle: Text('kPackageInfo.version'),
-              trailing: IconButton(
-                icon: const Icon(Icons.info),
-                onPressed: () {
-                  showAboutDialog(
-                    context: context,
-                    applicationName: 'MediTech',
-                    applicationVersion:' kPackageInfo.version',
-                    applicationIcon:Image.asset('assets/imahes/appstore.png'),
-                    children: <Widget>[const Text('APP_DESCRIPTION')],
-                  );
-                },
+              leading: Image(image: AssetImage('assets/images/appstore.png'),height: 70,width: 70,),
+              title: const Text('MediTech Life'),
+              subtitle: Text('version : 1.0.5'),
+              trailing: Padding(
+                padding: EdgeInsets.only(right: 25),
+                child: IconButton(
+                  icon: const Icon(CupertinoIcons.info_circle_fill),iconSize: 35,color: Colors.lightBlue,
+                  onPressed: () {
+                    showAboutDialog(
+                      context: context,
+                      applicationName: 'MediTech Life',
+                      applicationVersion:'version : 1.0.5',
+                      applicationIcon:Image(image: AssetImage('assets/images/appstore.png'),height: 70,width: 70,),
+                      children: <Widget>[const Text('MediTech is a one-stop digital health service solution for the people of Bangladesh.')],
+                    );
+                  },
+                ),
               ),
             ),
-            ListTile(
-              leading: const Icon(Icons.code),
-              title: const Text('Source code on GitHub'),
-              onTap: () =>
-                  Navigator.of(context).pushNamed('/growth_inapp_review_ex'),
+            Padding(
+              padding: EdgeInsets.only(left: 25),
+              child: ListTile(
+                leading: const Icon(Icons.code),
+
+                title: const Text('Cheak out GitHub'),
+                onTap: () =>
+                    Navigator.of(context).pushNamed('/growth_inapp_review_ex'),
+              ),
             ),
-            ListTile(
-              leading: const Icon(Icons.bug_report),
-              title: const Text('Report issue on GitHub'),
-              onTap: () =>
-                  Navigator.of(context).pushNamed('/growth_inapp_review_ex'),
+            Padding(
+              padding: EdgeInsets.only(left: 25),
+              child: ListTile(
+                leading: const Icon(Icons.bug_report),
+                title: const Text('Report issue on GitHub'),
+                onTap: () =>
+                    Navigator.of(context).pushNamed('/growth_inapp_review_ex'),
+              ),
             ),
-            ListTile(
-              leading: const Icon(Icons.open_in_new),
-              title: const Text('Visit my website'),
-              onTap: () =>
-                  Navigator.of(context).pushNamed('/growth_inapp_review_ex'),
+            Padding(
+              padding: EdgeInsets.only(left: 25),
+              child: ListTile(
+                leading: const Icon(Icons.open_in_new),
+                title: const Text('Visit my website'),
+                onTap: () =>
+                    Navigator.of(context).pushNamed('/growth_inapp_review_ex'),
+              ),
             ),
-            ListTile(
-              leading: const Icon(Icons.shop),
-              title: const Text('Rate the app'),
-              onTap: () =>
-                  Navigator.of(context).pushNamed('/growth_inapp_review_ex'),
+            Padding(
+              padding: EdgeInsets.only(left: 25),
+              child: ListTile(
+                leading: const Icon(Icons.shop),
+                title: const Text('Rate the app'),
+                onTap: () =>
+                    Navigator.of(context).pushNamed('/growth_inapp_review_ex'),
+              ),
             ),
-            ListTile(
-              leading: const Icon(Icons.apps),
-              title: const Text('My Other Apps'),
-              onTap: () => Navigator.of(context).pushNamed('/growth_my_other_apps'),
+            Padding(
+              padding: EdgeInsets.only(left: 25),
+              child: ListTile(
+                leading: const Icon(Icons.apps),
+                title: const Text('My Other Apps'),
+                onTap: () => Navigator.of(context).pushNamed('/growth_my_other_apps'),
+              ),
             ),
 
           ],
--- lib/pages/settings.dart
+++ lib/pages/settings.dart
@@ -24,173 +24,166 @@ class _SettingsState extends State<Settings> {
         centerTitle: true,
       ),
       body:
-      Column(
-        children: [
-          Container(
-            child: SettingsList(
-              sections: [
-                SettingsSection(
-                  title: Text('Common'),
-                  tiles: <SettingsTile>[
-                    SettingsTile.navigation(
-                      onPressed: (context) {
-                        showCupertinoDialog(
-                            context: context,
-                            builder: (BuildContext context) => Theme(
-                              data: ThemeData.light(),
-                              child: CupertinoAlertDialog(
-                                title: Text(
-                                  'Select Language',
-                                  style: TextStyle(color: CupertinoColors.systemIndigo, fontSize: 18),
-                                ),
-                                actions: [
-                                  CupertinoDialogAction(
-                                    child: Text(
-                                      'Bengali',
-                                      style: TextStyle(color: CupertinoColors.black, fontSize: 18),
-                                    ),
-                                    onPressed: () {
-
-                                    },
-                                  ),
-                                  CupertinoDialogAction(
-                                    child: Text(
-                                      'English',
-                                      style: TextStyle(color: CupertinoColors.black, fontSize: 18),
-                                    ),
-                                    onPressed: () {
-                                    },
-                                  ),
-                                  CupertinoDialogAction(
-                                    child: Text(
-                                      'Hindi',
-                                      style: TextStyle(color: CupertinoColors.black, fontSize: 18),
-                                    ),
-                                    onPressed: () {
-                                    },
-                                  ),
-                                  CupertinoDialogAction(
-                                    child: Text(
-                                      'China',
-                                      style: TextStyle(color: CupertinoColors.black, fontSize: 18),
-                                    ),
-                                    onPressed: () {
-                                    },
-                                  ),
-                                  CupertinoDialogAction(
-                                    child: Text(
-                                      'Arabic',
-                                      style: TextStyle(color: CupertinoColors.black, fontSize: 18),
-                                    ),
-                                    onPressed: () {
-                                    },
-                                  ),
-                                  CupertinoDialogAction(
-                                    child: Text(
-                                      'Spanish',
-                                      style: TextStyle(color: CupertinoColors.black, fontSize: 18),
-                                    ),
-                                    onPressed: () {
-                                    },
-                                  ),
-                                  CupertinoDialogAction(
-                                    child: Text(
-                                      'Cancel',
-                                      style: TextStyle(color: CupertinoColors.systemRed, fontSize: 18,fontWeight: FontWeight.bold),
-                                    ),
-                                    onPressed: () {
-                                      Navigator.pop(context);
-                                    },
-                                  ),
-                                ],
+      SettingsList(
+        sections: [
+          SettingsSection(
+            title: Text('Common'),
+            tiles: <SettingsTile>[
+              SettingsTile.navigation(
+                onPressed: (context) {
+                  showCupertinoDialog(
+                      context: context,
+                      builder: (BuildContext context) => Theme(
+                        data: ThemeData.light(),
+                        child: CupertinoAlertDialog(
+                          title: Text(
+                            'Select Language',
+                            style: TextStyle(color: CupertinoColors.systemIndigo, fontSize: 18),
+                          ),
+                          actions: [
+                            CupertinoDialogAction(
+                              child: Text(
+                                'Bengali',
+                                style: TextStyle(color: CupertinoColors.black, fontSize: 18),
                               ),
-                            ));
+                              onPressed: () {
 
-                      },
-                      leading: Icon(Icons.language),
-                      title: Text('Language'),
-                      value: Text('English'),
+                          },
+                        ),
+                        CupertinoDialogAction(
+                          child: Text(
+                            'English',
+                            style: TextStyle(color: CupertinoColors.black, fontSize: 18),
+                          ),
+                          onPressed: () {
+                          },
+                        ),
+                        CupertinoDialogAction(
+                          child: Text(
+                            'Hindi',
+                            style: TextStyle(color: CupertinoColors.black, fontSize: 18),
+                          ),
+                          onPressed: () {
+                          },
+                        ),
+                        CupertinoDialogAction(
+                          child: Text(
+                            'China',
+                            style: TextStyle(color: CupertinoColors.black, fontSize: 18),
+                          ),
+                          onPressed: () {
+                          },
+                        ),
+                        CupertinoDialogAction(
+                          child: Text(
+                            'Arabic',
+                            style: TextStyle(color: CupertinoColors.black, fontSize: 18),
+                          ),
+                          onPressed: () {
+                          },
+                        ),
+                        CupertinoDialogAction(
+                          child: Text(
+                            'Spanish',
+                            style: TextStyle(color: CupertinoColors.black, fontSize: 18),
+                          ),
+                          onPressed: () {
+                          },
+                        ),
+                        CupertinoDialogAction(
+                          child: Text(
+                            'Cancel',
+                            style: TextStyle(color: CupertinoColors.systemRed, fontSize: 18,fontWeight: FontWeight.bold),
+                          ),
+                          onPressed: () {
+                            Navigator.pop(context);
+                          },
+                        ),
+                      ],
                     ),
-                    SettingsTile.switchTile(
-                      onToggle: (value) {},
-                      initialValue: false,
-                      leading: Icon(Icons.format_paint),
-                      trailing: CupertinoSwitch(
-                      value: isTrackColorSwitch,
-                      trackColor: getColorFromHex('#f2866c'),
-                      activeColor: getColorFromHex('#8998FF'),
-                        onChanged: (bool val) {
-                         isTrackColorSwitch = val;
-                           setState(() {});
-                           },
-                      ),
-                      title: Text('Enable Dark theme'),
-                    ),
-                    SettingsTile.switchTile(
-                      onToggle: (value) {},
-                      initialValue: true,
-                      leading: Icon(Icons.notifications_active_rounded),
-                      trailing:CupertinoSwitch(
-                         value: isDefault,
-                       onChanged: (bool val) {
-                     isDefault = val;
-                     setState(() {});
-                     },
-                      ),
-                      title: Text('Enable Notifications'),
-                    ),
-                  ],
-                ),
-                SettingsSection(
-                  title: Text('Security'),
-                  tiles: [
-                    SettingsTile(
-                      title: Text('Password'),
-                      value: Text('App Password'),
-                      leading: Icon(Icons.lock),
-                      onPressed: (BuildContext context) {},
-                    ),
+                  ));
 
-                     SettingsTile(title: Text('FingerPrint'),
-                       leading: Icon(Icons.fingerprint),
-                       value: Text('Use Fingerprint to open app'),
-                     )
-
-                    // SettingsTile.switchTile(
-                    //   title: 'Use fingerprint',
-                    //   leading: Icon(Icons.fingerprint),
-                    //   switchValue: true,
-                    //   onToggle: (value) {},
-                    // ),
-                  ],
-                ),
-                SettingsSection(
-                  title: Text('Account'),
-                  tiles: [
-                    SettingsTile(
-                      title: Text('Phone'),
-                      leading: Icon(Icons.phone),
-                      onPressed: (BuildContext context) {},
-                    ),
-                    SettingsTile(
-                      title: Text('Email'),
-                      leading: Icon(Icons.email),
-                      onPressed: (BuildContext context) {},
-                    ),
-                    SettingsTile(
-                      title: Text('Sign Out'),
-                      leading: Icon(Icons.logout_rounded),
-                      onPressed: (BuildContext context) {},
-                    ),
-
-                  ],
-                ),
-
-              ],
+            },
+            leading: Icon(Icons.language),
+            title: Text('Language'),
+            value: Text('English'),
+          ),
+          SettingsTile.switchTile(
+            onToggle: (value) {},
+            initialValue: false,
+            leading: Icon(Icons.format_paint),
+            trailing: CupertinoSwitch(
+            value: isTrackColorSwitch,
+            trackColor: getColorFromHex('#f2866c'),
+            activeColor: getColorFromHex('#8998FF'),
+              onChanged: (bool val) {
+               isTrackColorSwitch = val;
+                 setState(() {});
+                 },
             ),
+            title: Text('Enable Dark theme'),
+          ),
+          SettingsTile.switchTile(
+            onToggle: (value) {},
+            initialValue: true,
+            leading: Icon(Icons.notifications_active_rounded),
+            trailing:CupertinoSwitch(
+               value: isDefault,
+             onChanged: (bool val) {
+           isDefault = val;
+           setState(() {});
+           },
+            ),
+            title: Text('Enable Notifications'),
           ),
         ],
       ),
-    );
+      SettingsSection(
+        title: Text('Security'),
+        tiles: [
+          SettingsTile(
+            title: Text('Password'),
+            value: Text('App Password'),
+            leading: Icon(Icons.lock),
+            onPressed: (BuildContext context) {},
+          ),
+
+           SettingsTile(title: Text('FingerPrint'),
+             leading: Icon(Icons.fingerprint),
+             value: Text('Use Fingerprint to open app'),
+           )
+
+          // SettingsTile.switchTile(
+          //   title: 'Use fingerprint',
+          //   leading: Icon(Icons.fingerprint),
+          //   switchValue: true,
+          //   onToggle: (value) {},
+          // ),
+        ],
+      ),
+      SettingsSection(
+        title: Text('Account'),
+        tiles: [
+          SettingsTile(
+            title: Text('Phone'),
+            leading: Icon(Icons.phone),
+            onPressed: (BuildContext context) {},
+          ),
+          SettingsTile(
+            title: Text('Email'),
+            leading: Icon(Icons.email),
+            onPressed: (BuildContext context) {},
+          ),
+          SettingsTile(
+            title: Text('Sign Out'),
+            leading: Icon(Icons.logout_rounded),
+            onPressed: (BuildContext context) {},
+          ),
+
+        ],
+      ),
+
+      ],)
+        );
   }
 }
