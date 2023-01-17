7f06b2ea0718698e564d26a7035c9914383724e9
Maruf Ahmed <mk6116055@gmail.com>
2023-01-18 02:17:12 +0600

    medicine page

--- lib/pages/medicine/medicine.dart
+++ lib/pages/medicine/medicine.dart
@@ -12,11 +12,19 @@ class MedicineHomeState extends State<MedicineHome> {
   Widget build(BuildContext context) {
     return SafeArea(
       child: Scaffold(
-        body: ListView(
+        appBar: AppBar(
+          title: Text('Medicine'),
+          centerTitle: true,
+        ),
+        body: 
+        ListView(
+          physics: BouncingScrollPhysics(parent: AlwaysScrollableScrollPhysics()),
           padding: EdgeInsets.symmetric(vertical: 16, horizontal: 16),
           children: [
             Card1(),
             Card2(),
+            Card3(),
+
             10.height,
           ],
         ),
@@ -28,33 +36,46 @@ class MedicineHomeState extends State<MedicineHome> {
     return Padding( padding: const EdgeInsets.only(top: 4,left: 8,right: 8,bottom: 12),
       child: Stack( alignment: Alignment.center, children: [
           Container(width: context.width(), decoration: boxDecorationDefault(borderRadius: radius(40), color: context.cardColor),
-            padding: EdgeInsets.only(top: 80, bottom: 16, left: 6, right: 6),
+            padding: EdgeInsets.only(top: 80, bottom: 4, left: 6, right: 6),
             child: Column( mainAxisSize: MainAxisSize.min, mainAxisAlignment: MainAxisAlignment.center,  children: [
-                Text('Tesla Inc.', style: boldTextStyle(size: 20)), 8.height,
-                Text('last update Aug 14, 6 PM', style: secondaryTextStyle()), 20.height,],),), Positioned(top: 2, child: Container(
+                Text('Antibiotics.', style: boldTextStyle(size: 20)), 8.height,
+                Padding(padding: const EdgeInsets.only(left: 10,right: 8,bottom: 1),
+                  child: Text('Antibiotics are used to treat or prevent some types of bacterial infection.'
+                      'They work by killing bacteria or preventing them from spreading.', style: secondaryTextStyle()),
+                ), 20.height,],),), Positioned(top: 2, child: Container(
               decoration: boxDecorationDefault( borderRadius: radius(16),), height: 70, width: 80,
-              child: Icon(Icons.title, size: 50, color: Colors.red),),)],),);
+              child: Image.asset('assets/images/antibiotics.jpg',scale: 3,)
+          ,),)],),);
+  }
+  Widget Card2() {
+    return Padding( padding: const EdgeInsets.only(top: 4,left: 8,right: 8,bottom: 12),
+      child: Stack( alignment: Alignment.center, children: [
+          Container(width: context.width(), decoration: boxDecorationDefault(borderRadius: radius(40), color: context.cardColor),
+            padding: EdgeInsets.only(top: 80, bottom: 4, left: 6, right: 6),
+            child: Column( mainAxisSize: MainAxisSize.min, mainAxisAlignment: MainAxisAlignment.center,  children: [
+                Text('Hydrocodone.', style: boldTextStyle(size: 20)), 8.height,
+                Padding(padding: const EdgeInsets.only(left: 10,right: 8,bottom: 1),
+                  child: Text('This combination medication is used to relieve moderate to severe pain.It contains an opioid pain reliever  and a non-opioid pain reliever.', style: secondaryTextStyle()),
+                ), 20.height,],),), Positioned(top: 2, child: Container(
+              decoration: boxDecorationDefault( borderRadius: radius(16),), height: 70, width: 80,
+              child: Image.asset('assets/images/hydrocodone.jpg',scale: 3,)
+          ,),)],),);
+  }
+  Widget Card3() {
+    return Padding( padding: const EdgeInsets.only(top: 4,left: 8,right: 8,bottom: 12),
+      child: Stack( alignment: Alignment.center, children: [
+          Container(width: context.width(), decoration: boxDecorationDefault(borderRadius: radius(40), color: context.cardColor),
+            padding: EdgeInsets.only(top: 80, bottom: 4, left: 6, right: 6),
+            child: Column( mainAxisSize: MainAxisSize.min, mainAxisAlignment: MainAxisAlignment.center,  children: [
+                Text('Metformin.', style: boldTextStyle(size: 20)), 8.height,
+                Padding(padding: const EdgeInsets.only(left: 10,right: 8,bottom: 1),
+                  child: Text('Metformin, sold under the brand name Glucophage, among others, is the main first-line medication for the treatment of type 2 diabetes, particularly in people who are overweight.', style: secondaryTextStyle()),
+                ), 20.height,],),), Positioned(top: 2, child: Container(
+              decoration: boxDecorationDefault( borderRadius: radius(16),), height: 70, width: 80,
+              child: Image.asset('assets/images/Metformin.jpg',scale: 3,)
+          ,),)],),);
   }
 
-}
 
-AppStore appStore = AppStore();
-class AppStore{
-
-  Color textPrimaryColor = Color(0xFF212121);
-  Color iconColorPrimaryDark = Color(0xFF212121);
-  Color scaffoldBackground = Color(0xFFEBF2F7);
-  Color backgroundColor = Colors.black;
-  Color backgroundSecondaryColor = Color(0xFF131d25);
-  Color appColorPrimaryLightColor = Color(0xFFF9FAFF);
-  Color textSecondaryColor = Color(0xFF5A5C5E);
-  Color appBarColor = Colors.white;
-  Color iconColor = Color(0xFF212121);
-  Color iconSecondaryColor = Color(0xFFA8ABAD);
-  Color cardColor = Colors.white;
-  Color appColorPrimary = Color(0xFF1157FA);
-  Color scaffoldBackgroundColor = Color(0xFFEFEFEF);
-
-  AppStore();
 
 }
